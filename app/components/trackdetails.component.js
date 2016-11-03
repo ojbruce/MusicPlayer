955f81ee2d2292c2e4ee975b76d1ad3c32db408e
ojbruce <olivia.j.bruce@gmail.com>
2016-11-03 18:23:13 +0100

    changing track details with just the title

--- app/components/trackdetails.component.js
+++ app/components/trackdetails.component.js
@@ -1,16 +1,24 @@
 import React from 'react';
 
 class TrackDetails extends React.Component {
-  // Render
+    
+    
+    // Render
     render(){
         return(
           <div className="trackdetails">
             <img src={this.props.trackimg} />
+            <h3>{this.props.name}</h3>
             <h3>{this.props.title}</h3>
           </div>
         )
     }
+    
+    defaultProps (){
+       name:'John Doe'
+    }
 
 }
+
 // Export
 export default TrackDetails
